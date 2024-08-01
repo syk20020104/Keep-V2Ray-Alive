ecf78699a28adbbe79e13168d9cb200ab9fab1fa
syk20020104 <79139518+syk20020104@users.noreply.github.com>
2024-08-01 11:51:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ ssh_info_str = os.getenv('SSH_INFO', '[]')  # 从环境变量获取SSH信息
 hosts_info = json.loads(ssh_info_str)        # 解析JSON格式的SSH信息
 
 # 要执行的命令列表
-commands = ["pm2 start ./domains/v2ray/v2ray --name v2ray --run", "uname -a"]
+commands = ["pm2 start  ~/domains/v2ray/v2ray --name v2ray --run", "uname -a"]
 
 # 执行命令并获取结果
 results = ssh_execute_commands(hosts_info, commands)
